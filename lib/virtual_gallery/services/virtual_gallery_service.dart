7109b0a7011b720507635d3ecc7bb5632deeeb8c
Aliaksandr Viarshel <sashavershel@gmail.com>
2023-05-07 22:12:50 +0300

    add english data to virtual gallery

--- lib/virtual_gallery/services/virtual_gallery_service.dart
+++ lib/virtual_gallery/services/virtual_gallery_service.dart
@@ -1,13 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+import '../../settings/app_settings.dart';
 
 import 'picture_dto.dart';
 
 class VirtualGalleryService {
+  final _settings = Get.find<AppSettings>();
+  final _languageCodeCollectionPathMap = {
+    'ru': 'vr_gallery',
+    'en': 'vr_gallery_en'
+  };
+
   Future<List<PictureDto>> getPictures() async {
     QuerySnapshot querySnapshot =
-        await FirebaseFirestore.instance.collection('vr_gallery').get();
+        await FirebaseFirestore.instance.collection(getCollectionPath()).get();
     return querySnapshot.docs
         .map((e) => PictureDto.fromMap(e.data() as Map<String, dynamic>))
         .toList();
+  }
+
+  String getCollectionPath() {
+    final languageCode = _settings.currentLocale.languageCode;
+    return _languageCodeCollectionPathMap[languageCode]!;
   }
 }
