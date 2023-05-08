0ed7c3e9a289ed9b15c73cc15f29c2a2de194bbc
Aliaksandr Viarshel <sashavershel@gmail.com>
2023-05-08 18:51:43 +0300

    make route names camel cased

--- lib/navigation/routes.dart
+++ lib/navigation/routes.dart
@@ -1,13 +1,13 @@
 class Routes {
-  static const galleriesMap = '/galleries_map';
-  static const academicBuildings = '/academic_buildings';
+  static const galleriesMap = '/galleries';
+  static const academicBuildings = '/academicBuildings';
   static const libraries = '/libraries';
   static const favorites = '/favorites';
   static const cafes = '/cafes';
   static const dormitories = '/dormitories';
   static const architecture = '/architecture';
   static const leisure = '/leisure';
-  static const artStores = '/art_stores';
-  static const virtualGallery = '/virtual_gallery';
+  static const artStores = '/artStores';
+  static const virtualGallery = '/virtualGallery';
   static const settings = '/settings';
 }
