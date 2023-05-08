1bedbf495963112e386109cd9544e57cb4644114
Aliaksandr Viarshel <sashavershel@gmail.com>
2023-05-08 20:59:48 +0300

    remove redundant print

--- lib/map/bottom_sheet/place.dart
+++ lib/map/bottom_sheet/place.dart
@@ -44,7 +44,6 @@ class Place {
   }
 
   Future<void> navigateToMap(BuildContext context) async {
-    print(mapId);
     await Navigator.of(context).pushReplacementNamed(
       '/$mapId',
       arguments: this,
