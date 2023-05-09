582b2d0336c1cf6da9e12d009cddd4432fce75d2
Aliaksandr Viarshel <sashavershel@gmail.com>
2023-05-09 19:26:14 +0300

    add english places

--- lib/map/services/places_firebase_repository.dart
+++ lib/map/services/places_firebase_repository.dart
@@ -1,14 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
 
 import 'package:grsu_guide/map/services/places_repository.dart';
+
+import '../../settings/app_settings.dart';
 
 import 'place_dto.dart';
 
 class PlacesFirebaseRepository implements PlacesRepository {
+  final _settings = Get.find<AppSettings>();
+  final _languageCodeCollectionPathMap = {
+    'ru': 'places',
+    'en': 'places_en'
+  };
+
   @override
   Future<List<PlaceDto>> getPlaces() async {
     QuerySnapshot querySnapshot =
-        await FirebaseFirestore.instance.collection('places').get();
+        await FirebaseFirestore.instance.collection(_getCollectionPath()).get();
     return querySnapshot.docs
         .map((e) => PlaceDto.fromMap(e.data() as Map<String, dynamic>))
         .toList();
@@ -17,10 +26,15 @@ class PlacesFirebaseRepository implements PlacesRepository {
   @override
   Future<PlaceDto> getPlace(String placeId) async {
     final documentSnapshot = await FirebaseFirestore.instance
-        .collection('places')
+        .collection(_getCollectionPath())
         .doc(placeId)
         .get();
     var data = documentSnapshot.data();
     return PlaceDto.fromMap(data as Map<String, dynamic>);
+  }
+
+  String _getCollectionPath() {
+    final languageCode = _settings.currentLocale.languageCode;
+    return _languageCodeCollectionPathMap[languageCode]!;
   }
 }
